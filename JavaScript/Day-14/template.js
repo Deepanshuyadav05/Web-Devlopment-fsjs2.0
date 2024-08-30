85eda06feae0c7aa02d7477c7546855fc82861b8
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2024-08-30 22:17:53 +0530

    template

---
+++ JavaScript/Day-14/template.js
@@ -0,0 +1,5 @@
+let pencilprice=10;
+let erasorprice=5;
+//let output = "the total price is: "+(pencilprice + erasorprice) + "rupees.";
+let output=`the  total price is : ${pencilprice+erasorprice} rupees`;
+console.log(output);
